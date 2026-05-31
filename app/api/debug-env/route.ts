d16100ead03c0ae31e69676ae033c43058b174c3
justindavis227 <justindavis227@gmail.com>
2026-05-31 11:08:03 -0400

    Delete app/api/debug-env/route.ts

--- app/api/debug-env/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  return Response.json({
-    dashboard_password_set: !!process.env.DASHBOARD_PASSWORD,
-    dashboard_password_length: process.env.DASHBOARD_PASSWORD?.length ?? 0,
-    auth_secret_set: !!process.env.AUTH_SECRET,
-    node_env: process.env.NODE_ENV,
-  })
-}
